d7edbefd1c242461fbb48faf35835743790252fa
Peter <peter@gmail.com>
2023-06-10 01:02:07 -0600

    ok

---
+++ Microprofile/Ver4/01-HelloWorld/src/main/java/com/example/demo/ConfigApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+
+@ApplicationPath("/ms")
+
+
+public class ConfigApplication extends Application {
+}
